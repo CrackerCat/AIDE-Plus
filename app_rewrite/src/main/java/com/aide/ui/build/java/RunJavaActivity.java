62dbca1a6fdcc2a20b14534ed67c96901335e9f3
ZeroAicy <ZeroAicy@qq.com>
2025-03-04 13:05:23 +0800

    添加 Java控制台宿主模式

--- app_rewrite/src/main/java/com/aide/ui/build/java/RunJavaActivity.java
+++ app_rewrite/src/main/java/com/aide/ui/build/java/RunJavaActivity.java
@@ -20,9 +20,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.lang.reflect.Method;
+import io.github.zeroaicy.util.FileUtil;
+import io.github.zeroaicy.aide.preference.ZeroAicySetting;
 
 public class RunJavaActivity extends OutputConsoleActivity {
-
 
 	static Handler getUiHandler(RunJavaActivity runJavaActivity) {
 		return runJavaActivity.mb;
@@ -46,11 +47,10 @@ public class RunJavaActivity extends OutputConsoleActivity {
 
 	private Class<?> adrtClass;
 
-	static ThreadPoolService defaultThreadPoolService = ThreadPoolService.getDefaultThreadPoolService();
+	private static ThreadPoolService defaultThreadPoolService = ThreadPoolService.getDefaultThreadPoolService();
 
 	@Override
 	protected void XL() {
-
 
 		String extarDex = getIntent().getExtras().getString("EXTRA_DEX");
 
@@ -58,65 +58,68 @@ public class RunJavaActivity extends OutputConsoleActivity {
 
 		boolean extarDebug = getIntent().getBooleanExtra("EXTRA_DEBUG", false);
 		File optimizedDirectory = getDir("outdex", 0);
-		
+
 		File extarDexFile = new File(extarDex);
 		File extarDexOutFile = new File(optimizedDirectory, extarDexFile.getName());
 
-		
-		// extarDex -> extarDexOutFile
-		RunMainMethodRunnable runMainMethodRunnable = new RunMainMethodRunnable(this, extarDexOutFile.getAbsolutePath(), extarClass, optimizedDirectory, extarDebug);
-		
-		CopyDexRunable copyDexRunable = new CopyDexRunable(this, extarDex, extarDexOutFile, optimizedDirectory, runMainMethodRunnable);
-		
+		// 复制dex.jar到 私有目录
+		CopyDexRunable copyDexRunable = new CopyDexRunable(this, extarDexFile, extarDexOutFile, optimizedDirectory,
+				extarClass, extarDebug);
+
 		defaultThreadPoolService.submit(copyDexRunable);
 
 	}
 
 	// 主线程中运行
-	private void runMainMethod(String extarDex, String extarClass, File optimizedDirectory, boolean extarDebug) {
-		DexClassLoader dexClassLoader = new DexClassLoader(extarDex, optimizedDirectory.getPath(), null,
-														   ClassLoader.getSystemClassLoader());
+	private void runMainMethod(String extarDex, String optimizedDirectory, String extarClass, boolean extarDebug) {
 		
+		boolean isEnableJavaConsoleHostMode = ZeroAicySetting.isEnableJavaConsoleHostMode();
+		ClassLoader parent = isEnableJavaConsoleHostMode ? this.getClassLoader() : ClassLoader.getSystemClassLoader();
+		DexClassLoader dexClassLoader = new DexClassLoader(extarDex, optimizedDirectory, null,
+														   parent);
 		// 调试模式
 		if (extarDebug) {
 			try {
 				this.adrtClass = dexClassLoader.loadClass("adrt/ADRT");
 				this.adrtClass
-					.getDeclaredMethod("connectDebugger", Context.class, String.class, Boolean.TYPE, Boolean.TYPE)
-					.invoke(null, getApplicationContext(), getPackageName(), Boolean.FALSE, Boolean.FALSE);
-			}
-			catch (Throwable e) {
+						.getDeclaredMethod("connectDebugger", Context.class, String.class, Boolean.TYPE, Boolean.TYPE)
+						.invoke(null, getApplicationContext(), getPackageName(), Boolean.FALSE, Boolean.FALSE);
+			} catch (Throwable e) {
 				e.printStackTrace();
 			}
 		}
 
 		try {
-			Method declaredMethod = dexClassLoader.loadClass(extarClass).getDeclaredMethod("main", String[].class);
+
 			OutputConsole outputConsole = this.WB;
 
+			// 设置 控制台输入输出流
 			System.setOut(outputConsole.getPrintStream());
 			System.setErr(outputConsole.getPrintStream());
 			System.setIn(outputConsole.getInputStream());
+			
+			
+			Method mainMethod = dexClassLoader.loadClass(extarClass).getDeclaredMethod("main", String[].class);
 
 			// 异步 运行 main方法
-			defaultThreadPoolService.submit(new InvokeMainMethodRunnable(this, declaredMethod, new String[0]));
+			defaultThreadPoolService.submit(new InvokeMainMethodRunnable(this, mainMethod, new String[0]));
 
-		}
-		catch (Throwable e) {
+		} catch (Throwable e) {
 			e.printStackTrace();
 		}
 	}
 
+	@Override
 	protected void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 	}
 
+	@Override
 	protected void onDestroy() {
 		if (this.adrtClass != null) {
 			try {
 				this.adrtClass.getDeclaredMethod("disconnectDebugger", new Class[0]).invoke(null, new Object[0]);
-			}
-			catch (Throwable th) {
+			} catch (Throwable th) {
 				th.printStackTrace();
 			}
 		}
@@ -126,6 +129,7 @@ public class RunJavaActivity extends OutputConsoleActivity {
 		Process.killProcess(Process.myPid());
 	}
 
+	@Override
 	protected int u7() {
 		// 0x7f07007b
 		return R.drawable.ic_launcher_java;
@@ -134,71 +138,90 @@ public class RunJavaActivity extends OutputConsoleActivity {
 	public static class CopyDexRunable implements Runnable {
 
 		RunJavaActivity runJavaActivity;
-		String extarDex;
+		File extarDexFile;
 		File optimizedDirectory;
-		Runnable uiSyncRunnable;
-		
+
 		File extarDexOutFile;
-		
-		public CopyDexRunable(RunJavaActivity runJavaActivity, String extarDex, File extarDexOutFile, File optimizedDirectory, Runnable uiSyncRunnable) {
+
+		String extarClass;
+		boolean extarDebug;
+
+		public CopyDexRunable(RunJavaActivity runJavaActivity, File extarDexFile, File extarDexOutFile,
+				File optimizedDirectory, String extarClass, boolean extarDebug) {
 			this.runJavaActivity = runJavaActivity;
-			this.extarDex = extarDex;
+			this.extarDexFile = extarDexFile;
 			this.extarDexOutFile = extarDexOutFile;
 			this.optimizedDirectory = optimizedDirectory;
 
-			this.uiSyncRunnable = uiSyncRunnable;
+			this.extarClass = extarClass;
+			this.extarDebug = extarDebug;
+
 		}
 
 		@Override
 		public void run() {
 
-			if (!optimizedDirectory.exists()) optimizedDirectory.mkdirs();
+			// 创建 存放dex.zip 缓存目录
+			if (!this.optimizedDirectory.exists()) {
+				this.optimizedDirectory.mkdirs();
+			}
 
-			File[] files = optimizedDirectory.listFiles();  
-			if (files != null) {
-				for (File file : files) {
-					file.delete();
+			if (this.extarDexOutFile.isDirectory()) {
+				FileUtil.deleteFolder(this.extarDexOutFile);
+			}
+
+			if (this.extarDexOutFile.lastModified() <= this.extarDexFile.lastModified()) {
+
+				// 删除
+				this.extarDexOutFile.delete();
+
+				// 复制文件到
+				FileInputStream input = null;
+				FileOutputStream output = null;
+				try {
+					input = new FileInputStream(this.extarDexFile);
+					output = new FileOutputStream(this.extarDexOutFile);
+					IOUtils.streamTransfer(input, output);
+				} catch (Throwable e) {
+				} finally {
+					IOUtils.close(input);
+					IOUtils.close(output);
 				}
-			}
-			FileInputStream input = null;
-			FileOutputStream output = null;
-			try {
-				input = new FileInputStream(this.extarDex);
-				output = new FileOutputStream(this.extarDexOutFile);
-				IOUtils.streamTransfer(input, output);
-			}
-			catch (Throwable e) {}
-			finally {
-				IOUtils.close(input);
-				IOUtils.close(output);
 			}
 
 			// 适配 Android 14 -> DexClassLoader 不再支持从可写文件加载 dex/jar 文件
 			this.extarDexOutFile.setWritable(false, false);
 
+			// extarDex -> extarDexOutFile
+			RunMainMethodRunnable runMainMethodRunnable = new RunMainMethodRunnable(this.runJavaActivity,
+					extarDexOutFile.getAbsolutePath(), optimizedDirectory.getAbsolutePath(), this.extarClass,
+					this.extarDebug);
+			// 调用 入口函数
 			Handler uiHandler = RunJavaActivity.getUiHandler(this.runJavaActivity);
-			uiHandler.post(this.uiSyncRunnable);
+			uiHandler.post(runMainMethodRunnable);
 		}
 	}
-	
+
 	public static class RunMainMethodRunnable implements Runnable {
 		RunJavaActivity runJavaActivity;
 		String extarDex;
+		String optimizedDirectory;
 		String extarClass;
-		File optimizedDirectory;
 		boolean extarDebug;
 
-		public RunMainMethodRunnable(RunJavaActivity runJavaActivity, String extarDex, String extarClass, File optimizedDirectory, boolean extarDebug) {
+		public RunMainMethodRunnable(RunJavaActivity runJavaActivity, String extarDex, String optimizedDirectory,
+				String extarClass, boolean extarDebug) {
 			this.runJavaActivity = runJavaActivity;
 			this.extarDex = extarDex;
-			this.extarClass = extarClass;
 			this.optimizedDirectory = optimizedDirectory;
+
+			this.extarClass = extarClass;
 			this.extarDebug = extarDebug;
 		}
 
 		@Override
 		public void run() {
-			runJavaActivity.runMainMethod(extarDex, extarClass, optimizedDirectory, extarDebug);
+			this.runJavaActivity.runMainMethod(extarDex, optimizedDirectory, extarClass, extarDebug);
 		}
 
 	}
@@ -218,8 +241,7 @@ public class RunJavaActivity extends OutputConsoleActivity {
 		public void run() {
 			try {
 				this.curMethod.invoke(null, this.args);
-			}
-			catch (Throwable th) {
+			} catch (Throwable th) {
 				if (th.getCause() != null) {
 					th.getCause().printStackTrace();
 				} else {
