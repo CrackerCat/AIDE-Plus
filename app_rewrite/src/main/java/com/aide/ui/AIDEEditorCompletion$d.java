7ebd1be5544491976c0be1fd142e7f6843cac07a
ZeroAicy <ZeroAicy@qq.com>
2025-06-23 20:43:40 +0800

    去除 代码提示条目长按事件

---
+++ app_rewrite/src/main/java/com/aide/ui/AIDEEditorCompletion$d.java
@@ -0,0 +1,22 @@
+/**
+ * @Date 
+ * @AIDE AIDE+ 
+ */
+package com.aide.ui;
+
+import android.view.View;
+import android.widget.AdapterView;
+import com.aide.ui.views.CompletionListView;
+
+public class AIDEEditorCompletion$d implements AdapterView.OnItemLongClickListener {
+	public AIDEEditorCompletion$d(AIDEEditorCompletion aideEditorCompletion, CompletionListView completionListView) {
+
+	}
+
+	@Override
+	public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+		return false;
+	}
+
+}
+
