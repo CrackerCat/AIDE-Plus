411595f0810daac559f45e9e027ed5998f479c89
ZeroAicy <ZeroAicy@qq.com>
2025-06-25 23:13:39 +0800

    重写QuickKeysBar准备

---
+++ app_rewrite/src/main/java/com/aide/ui/QuickKeysBar.java
@@ -0,0 +1,168 @@
+/**
+ * @Date 
+ * @AIDE AIDE+ 
+ */
+package com.aide.ui;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.Keep;
+import com.aide.common.AndroidHelper;
+import com.aide.ui.MainActivity;
+import com.aide.ui.rewrite.R;
+
+@Keep
+public class QuickKeysBar {
+	
+	QuickKeysBarFormAide quickKeysBarFormAide;
+	
+	@Keep
+	public QuickKeysBar(MainActivity mainActivity) {
+		quickKeysBarFormAide = new QuickKeysBarFormAide(mainActivity);
+	}
+
+	@Keep
+	public void gn(String s) {
+		quickKeysBarFormAide.gn(s);
+	}
+
+	@Keep
+	public int v5() {
+		return quickKeysBarFormAide.v5();
+	}
+
+	@Keep
+	public void show(boolean show) {
+		quickKeysBarFormAide.show(show);
+	}
+
+	public static class QuickKeysBarFormAide {
+
+		private String FH;
+
+		private boolean Hw;
+
+		private MainActivity mainActivity;
+
+		private View quickkeysbarView;
+
+		private KeyCharacterMap v5;
+
+		public QuickKeysBarFormAide(MainActivity mainActivity) {
+			this.FH = "";
+			this.mainActivity = mainActivity;
+			this.quickkeysbarView = LayoutInflater.from(mainActivity).inflate(R.layout.quickkeysbar,
+					mainActivity.findViewById(0x7f080120));
+			VH(AndroidHelper.getVerticalScreenWidthInDp(mainActivity) >= 360.0f
+					&& AndroidHelper.isNotTelevisionMode(mainActivity));
+			this.quickkeysbarView.findViewById(R.id.quickKeyBarOpenButton)
+					.setOnClickListener(new View.OnClickListener() {
+						@Override
+						public void onClick(View v) {
+							j6(QuickKeysBarFormAide.this, true);
+						}
+					});
+			this.quickkeysbarView.findViewById(R.id.quickKeyBarCloseButton)
+					.setOnClickListener(new View.OnClickListener() {
+						@Override
+						public void onClick(View v) {
+							QuickKeysBarFormAide.j6(QuickKeysBarFormAide.this, false);
+						}
+					});
+		}
+
+		static KeyCharacterMap DW(QuickKeysBarFormAide quickKeysBar) {
+			return quickKeysBar.v5;
+		}
+
+		static KeyCharacterMap FH(QuickKeysBarFormAide quickKeysBar, KeyCharacterMap keyCharacterMap) {
+			quickKeysBar.v5 = keyCharacterMap;
+			return keyCharacterMap;
+		}
+
+		static MainActivity Hw(QuickKeysBarFormAide quickKeysBar) {
+			return quickKeysBar.mainActivity;
+		}
+
+		private void VH(boolean z) {
+			this.Hw = z;
+			if (z) {
+				this.quickkeysbarView.findViewById(R.id.quickKeyBarOpenButtonContainer).setVisibility(8);
+				this.quickkeysbarView.findViewById(R.id.quickKeyBarKeysContainer).setVisibility(0);
+			} else {
+				this.quickkeysbarView.findViewById(R.id.quickKeyBarOpenButtonContainer).setVisibility(0);
+				this.quickkeysbarView.findViewById(R.id.quickKeyBarKeysContainer).setVisibility(8);
+			}
+		}
+
+		static void j6(QuickKeysBarFormAide quickKeysBar, boolean z) {
+			quickKeysBar.VH(z);
+		}
+
+		public void gn(String str) {
+			float f;
+			float f2;
+
+			if (this.quickkeysbarView == null || str == null || this.FH.equals(str)) {
+				return;
+			}
+			this.FH = str;
+			LayoutInflater from = LayoutInflater.from(this.mainActivity);
+			if (AndroidHelper.getVerticalScreenWidthInDp(this.mainActivity) >= 400.0f) {
+				f = 60.0f;
+				f2 = this.mainActivity.getResources().getDisplayMetrics().density;
+			} else {
+				f = 30.0f;
+				f2 = this.mainActivity.getResources().getDisplayMetrics().density;
+			}
+			int i = (int) (f2 * f);
+			int i2 = (int) (this.mainActivity.getResources().getDisplayMetrics().density * 40.0f);
+			ViewGroup viewGroup = (ViewGroup) this.quickkeysbarView.findViewById(R.id.quickKeyBarList);
+			viewGroup.removeAllViews();
+			for (String str2 : str.split(" ")) {
+				String replace = str2.replace("s", " ");
+				TextView textView = (TextView) from.inflate(R.layout.quickkeysbar_key, (ViewGroup) null);
+				if (replace.trim().length() == 0) {
+					textView.setText("⇥");
+				} else {
+					textView.setText(replace);
+				}
+				viewGroup.addView(textView, new LinearLayout.LayoutParams(i, i2));
+				textView.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						if (QuickKeysBarFormAide.DW(QuickKeysBarFormAide.this) == null) {
+							QuickKeysBarFormAide.FH(QuickKeysBarFormAide.this, KeyCharacterMap.load(-1));
+						}
+						KeyEvent[] events = QuickKeysBarFormAide.DW(QuickKeysBarFormAide.this).getEvents(str.toCharArray());
+						if (events != null) {
+							for (KeyEvent keyEvent : events) {
+								QuickKeysBarFormAide.Hw(QuickKeysBarFormAide.this).dispatchKeyEvent(keyEvent);
+							}
+						}
+					}
+				});
+			}
+
+		}
+
+		public void show(boolean z) {
+			if (this.quickkeysbarView != null) {
+				this.quickkeysbarView.findViewById(0x7f08013f).setVisibility(z ? 0 : 8);
+			}
+		}
+
+		public int v5() {
+			if (this.Hw) {
+				return (int) (this.mainActivity.getResources().getDisplayMetrics().density * 40.0f);
+			}
+			return 0;
+		}
+	}
+
+}
+
